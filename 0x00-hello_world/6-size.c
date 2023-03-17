e9e6e63d663de739b9df0b78b37e530a508a394c
Your Name <you@example.com>
2023-03-16 18:56:22 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char: %lu byt)\n", sizeof(a));
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of an int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
