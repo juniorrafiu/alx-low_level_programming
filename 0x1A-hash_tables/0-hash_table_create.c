3e0edb5e47cb16f8ec3cb9e93b7d8fcceea0964f
juniorrafiu <juniorrafiu7@gmail.com>
2023-12-05 03:49:07 -0800

    hard

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates an empty hashtable
+ * @size: size of the array in the hashtable
+ * Return: new hashtable or null if any failure
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+	{
+		return (NULL);
+	}
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	hashtable->size = size;
+	return (hashtable);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives the index of a key
+ * @size: size of the array in the hashtable
+ * @key: key
+ * Return: value of the index of the related key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int value = 0;
+
+	value = hash_djb2(key);
+	value = value % size;
+	return (value);
+}
